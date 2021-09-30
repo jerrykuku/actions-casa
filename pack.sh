505fc30a1025be2484da8b86a0507740936ce787
老竭力 <jerrykuku@qq.com>
2021-09-30 16:55:57 +0800

    Update pack.sh

--- pack.sh
+++ pack.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
-final_path=$1
-ls
-#sudo mkdir -p $final_path
-pack_path=casaos
-path=build
+PACK_PATH=casaos
+BUILD_PATH=build
+SUFFIX="-casaos"
 files=$(ls $path)
-suffix="-casaos"
 for filename in $files
 do
-  sudo rm -rf $pack_path
-  sudo mkdir -p $pack_path  
+  sudo rm -rf $PACK_PATH
+  sudo mkdir -p $PACK_PATH  
   arch=$(echo $filename | sed 's/casa-//g')
-  package_name=$arch$suffix
-  upx --lzma --best $path/$filename
-  sudo cp -r casa/conf $pack_path
-  sudo cp -r casa/shell $pack_path
-  sudo cp -rf $path/$filename $pack_path/casaos
-  sudo tar zcvf "${package_name}.tar.gz" casaos
+  package_name=$arch$SUFFIX
+  upx --lzma --best $BUILD_PATH/$filename
+  sudo cp -r casa/conf $PACK_PATH
+  sudo cp -r casa/shell $PACK_PATH
+  sudo cp -rf $BUILD_PATH/$filename $PACK_PATH/casaos
+  sudo tar zcvf "${package_name}.tar.gz" $PACK_PATH
   ls
 done
